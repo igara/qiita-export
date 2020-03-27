6cdc250c26f0dfa7ac8590bd1e43af2f6895e646
garasyo <igara1119@gmail.com>
2020-03-28 01:46:46 +0900

    add: html style.

--- html.ts
+++ html.ts
@@ -1,11 +1,19 @@
 import * as childProcess from "child_process";
 import * as fs from "fs";
+import * as highlight from "highlight.js";
 import * as marked from "marked";
+
+const highlightStyle = fs.readFileSync("./node_modules/highlight.js/styles/railscasts.css", "utf8");
+const markdownStyle = fs.readFileSync("./node_modules/github-markdown-css/github-markdown.css", "utf8");
+const styleTag = `<style>
+${highlightStyle}
+${markdownStyle}
+</style>`;
 
 const exec = () => {
   const gitUrl = childProcess.execSync("git config --get remote.origin.url").toString();
   const repositoryName = gitUrl.replace(/(https:\/\/github.com\/|git@github.com:|.git)/g, "");
-  console.log(repositoryName);
+
   const qiitaIDDirectoryNames = fs.readdirSync("data");
   qiitaIDDirectoryNames.forEach((qiitaIDDirectoryName) => {
     const qiitaItemDirectoryNames = fs.readdirSync(`data/${qiitaIDDirectoryName}`);
@@ -13,16 +21,22 @@ const exec = () => {
       if (/^\./.test(qiitaItemDirectoryName)) return;
 
       const markdown = fs.readFileSync(`data/${qiitaIDDirectoryName}/${qiitaItemDirectoryName}/README.md`).toString();
-      let html = marked(markdown, {
-        // baseUrl: ``,
-        // baseUrl: "",
-        gfm: false,
+      let body = marked(markdown, {
+        gfm: true,
+        highlight: (code) => {
+          return highlight.highlightAuto(code).value;
+        },
       });
-      html = html.replace(
+      body = body.replace(
         /src="/g,
         `src="https://raw.githubusercontent.com/${repositoryName}/master/data/${qiitaIDDirectoryName}/${qiitaItemDirectoryName}/`,
       );
-      fs.writeFileSync(`data/${qiitaIDDirectoryName}/${qiitaItemDirectoryName}/README.html`, html);
+
+      body = `<body class="markdown-body">
+  ${body}
+</body>`;
+
+      fs.writeFileSync(`data/${qiitaIDDirectoryName}/${qiitaItemDirectoryName}/README.html`, styleTag + body);
     });
   });
 };
